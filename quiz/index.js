fbf2a2c49391b80ce2605a8d0ce6e2c262e96695
Arpit Agarwa1 <arpitagrwal932@gmail.com>
2025-08-05 23:47:13 +0530

    updTING CSS

--- quiz/index.js
+++ quiz/index.js
@@ -8,14 +8,14 @@ const data = [
   },
 
   {
-    question: "WHat is the capital of India?",
+    question: "What is the capital of India?",
     answer: "New Delhi",
     options: ["New Delhi", "Canberra", "Queensland", "Perth"],
   },
 
   {
     question: "What is the capital of Israel?",
-    answer: "Tel Aviv",
+    answer: "Jeruslam",
     options: ["Tel Aviv", "Jeruslam", "Queensland", "Perth"],
   },
 
@@ -26,40 +26,24 @@ const data = [
   },
 ];
 
-// const quiz = document.querySelector("#quiz");
-// const question = document.querySelector(".question");
-// const ansOptions = document.querySelectorAll(".option");
-// const button = document.querySelector("button");
-// const divOption = document.querySelector(".options");
-// const para = document.querySelector("p");
-
-// const myInterval = setInterval(startQuiz, 1000);
-// let i = 0;
-// let count = 5;
-// button.addEventListener("click", startQuiz);
-// function startQuiz() {
-//   button.style.display = "none";
-//   quiz.style.display = "block";
-//   question.innerText = data[i].question;
-//   for (let j = 0; j < ansOptions.length; j++) {
-//     ansOptions[j].innerText = data[i].options[j];
-//   }
-//   para.innerText = count--;
-//   if (count < 0) {
-//     count = 5;
-// }
-// }
-
 let button = document.querySelector("button");
 let quiz = document.querySelector("#quiz");
 let question = document.querySelector(".question");
 let divOptions = document.querySelectorAll(".option");
 let para = document.querySelector("#para");
+let score = document.querySelector(".score");
+let correct = document.querySelector(".correct");
+let wrong = document.querySelector(".wrong");
+let list = document.querySelector(".list");
+let scoreHeading = document.querySelector(".score-heading");
+let answerQuestion = document.querySelectorAll(".answer-key-question");
+let answerKeyanswers = document.querySelectorAll(".answer-key-answer");
 
 let clickEvent = button.addEventListener("click", Ignition);
 
 let i = 0;
 let count = 5;
+let countdown;
 
 function Ignition() {
   startQuiz();
@@ -68,20 +52,60 @@ function Ignition() {
 
 function startQuiz() {
   button.style.display = "none";
+
   quiz.style.display = "block";
+
   if (i < data.length) {
     question.innerText = data[i].question;
+
     for (let j = 0; j < divOptions.length; j++) {
       divOptions[j].innerText = data[i].options[j];
     }
 
     para.innerText = count--;
+    // answerQuestion.innerText = data[i].question;
+    // answerKeyanswers.innerText = data[i].answer;
   } else {
     clearInterval(countdown);
+
+    for (let a = 0; a < answerQuestion.length; a++) {
+      answerQuestion[a].innerText = data[a].question;
+
+      answerQuestion[a].style.display = "block";
+    }
+
+    for (let b = 0; b < answerKeyanswers.length; b++) {
+      answerKeyanswers[b].innerText = data[b].answer;
+      answerKeyanswers[b].style.display = "block";
+    }
+
+    button.style.display = "none";
+    para.style.display = "none";
+    quiz.style.display = "none";
+    score.style.display = "block";
+    scoreHeading.style.display = "block";
   }
 
   if (count <= 0) {
     count = 5;
     i++;
+  }
+}
+
+let answerKEy = 0;
+let correctAnswer = [];
+let wrongAnswer = [];
+
+for (let k = 0; k < divOptions.length; k++) {
+  divOptions[k].addEventListener("click", answer);
+  function answer() {
+    if (divOptions[k].innerText == data[i].answer) {
+      correctAnswer.push(divOptions[k].innerText);
+      ++answerKEy;
+
+      score.innerHTML = answerKEy;
+    } else {
+      wrongAnswer.push(divOptions[k].innerText);
+    }
   }
 }
